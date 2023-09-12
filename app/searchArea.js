a59a4ec7960e3c9f67f0999cc24c48075ee937b6
yongwoon Jang <royalfamily89@gmail.com>
2023-09-12 20:24:13 +0900

    add: new pic

--- app/searchArea.js
+++ app/searchArea.js
@@ -36,6 +36,7 @@ function getSuggestionComponent({ suggestionList, choiceNum, setChoiceNum }) {
 
 export default function SearchArea({ isFocus, setFocus }) {
     const data = [
+        { category: "album", title: "안방을 정리하는 아기", url: "/images/family/20230912_son_안방을 정리하는 아기.jpg"},
         { category: "album", title: "배드민턴 감독 장하성", url: "/images/family/20230911_son_배드민턴 감독.jpeg"},
         { category: "album", title: "장하성은 엄마를 좋아한다.", url: "/images/family/20230911_son_성남외고_행복한 세상의 장하성.jpeg" },
         { category: "album", title: "약사 장하성", url: "/images/family/20230908_son_약사_옷방_귀여움.jpeg" },
