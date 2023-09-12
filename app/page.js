5cd44189322fff35219343b72b0722edbe34c0de
yongwoon Jang <royalfamily89@gmail.com>
2023-09-12 21:01:37 +0900

    style: make name more readable

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [isFocus, setFocus] = useState(false);
 
   return (
-    <div onClick={()=>{setFocus(false)}} className={styles.wrap}>
+    <div onClick={()=>{setFocus(false)}} className={styles.background}>
       <main className={isFocus ? `${styles.main} ${styles['main--focus']}` : `${styles.main}`}>
         <SearchArea isFocus={isFocus} setFocus={setFocus}/>
       </main>
