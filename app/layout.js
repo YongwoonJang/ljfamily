c9a830d78c7b0752aa1c24a729ef4c383876a00f
Yongwoon Jang <royalfamily89@gmail.com>
2023-09-15 15:06:45 +0900

    style: change title

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './layout.css'
 
 export const metadata = {
-  title: 'LJ',
+  title: 'The LJ',
   description: '2023년 9월 1일부터 시작한 "이&장" 가족의 홈페이지 입니다.',
   keywords: "가족, 장용운, 홈페이지",
   openGraph: {
