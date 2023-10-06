78a741bdee1cd2c6ffb688281d53ca5ede52ca02
yongwoon Jang <royalfamily89@gmail.com>
2023-10-06 21:31:35 +0900

    fix: bug

--- app/album/[slug]/page.js
+++ app/album/[slug]/page.js
@@ -2,21 +2,14 @@ import getDatabase from "../../lib/database";
 import styles from "@/app/album/[slug]/page.module.css"
 
 export async function generateStaticParams(){
-    const cont = getDatabase().filter((el)=>{return el.category=='album'});
-    return cont.map((el)=>{return {slug: encodeURI(el.title)}});
+    const cont = getDatabase().filter((el)=>{return el.category==='album'});
+    return cont.map((el)=>{return {slug: el.title}});
 }
 
 export default function Page({params}){
     const data = getDatabase();
-    const pic = data.filter((el)=>{return encodeURI(el.title) == params.slug})[0];
-    console.log("===============");
-    console.log(data[0].title);
-    console.log(params.slug);
-    console.log(decodeURI(params.slug));
-    console.log("Compare between two data: "+encodeURI(data[0].title)+" : "+params.slug);
-    console.log(pic);
-    console.log("===============")
-    
+    const pic = data.filter((el)=>{return encodeURI(el.title) === params.slug})[0];
+
     return (
         <div className={styles['picture-frame']}>
             <div>{pic?.title}</div>
