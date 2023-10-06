2f2658f58794371ae24be8120a7bc372cc33e13c
yongwoon Jang <royalfamily89@gmail.com>
2023-10-06 21:59:06 +0900

    apply: responsive design

--- app/album/[slug]/page.js
+++ app/album/[slug]/page.js
@@ -1,5 +1,7 @@
+"use client"
 import getDatabase from "../../lib/database";
 import styles from "@/app/album/[slug]/page.module.css"
+import { useRouter } from "next/navigation";
 
 export async function generateStaticParams(){
     const cont = getDatabase().filter((el)=>{return el.category==='album'});
@@ -9,11 +11,18 @@ export async function generateStaticParams(){
 export default function Page({params}){
     const data = getDatabase();
     const pic = data.filter((el)=>{return encodeURI(el.title) === params.slug})[0];
+    const router = useRouter();
 
     return (
         <div className={styles['picture-frame']}>
-            <div>{pic?.title}</div>
+            <div>제목: {pic?.title}</div>
             <img className={styles['picture-frame__picture']} src={pic?.url}/>
+            <button
+                className={styles['picture-frame__button']}
+                onClick={()=>{
+                    router.push("https://ljfamily.vercel.app/");
+                }}
+            >뒤로 가기</button>
         </div>
     )
 
